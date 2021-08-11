699e391f6a4b4e760fd988f081c58ff74b135c32
Thales <thsm271@gmail.com>
2021-08-10 22:46:50 -0300

    second Commit

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Questions from "./Questions";
+import Questions from "./Questions/Questions";
 
 function HomeScreen() {
   return <Questions />;
--- src/screens/Questions/Questions.tsx
+++ src/screens/Questions/Questions.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { HandleRandomQuestion } from "../api/axios";
+import { HandleRandomQuestion } from "../../api/axios";
 
 import {
   StyledDiv,
